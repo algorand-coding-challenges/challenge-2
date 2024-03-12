e635f63d9374f55cef094fbb589476ec63076642
dex0id <126304247+dex0id@users.noreply.github.com>
2024-03-12 09:57:30 -0400

    adding dependencies and fixing file path in compile-contract script

--- challenge/contracts/algorand-puzzle-2.algo.ts
+++ challenge/contracts/algorand-puzzle-2.algo.ts
@@ -1,10 +1,10 @@
 import { Contract } from '@algorandfoundation/tealscript';
 
 // TODO: Uncomment Line 8 before solving the puzzle.
-
+console.log('hit');
 // eslint-disable-next-line no-unused-vars
 class AlgorandPuzzle2 extends Contract {
   solveThePuzzle(): string {
-    // return 'You solved the puzzle!';
+    return 'You solved the puzzle!';
   }
 }
