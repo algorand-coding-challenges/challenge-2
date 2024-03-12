73dd809eb132e9e8e0b880aafa7d3dc7fdcea7b4
dex0id <126304247+dex0id@users.noreply.github.com>
2024-03-12 10:05:15 -0400

    removing console.log

--- challenge/contracts/algorand-puzzle-2.algo.ts
+++ challenge/contracts/algorand-puzzle-2.algo.ts
@@ -1,7 +1,6 @@
 import { Contract } from '@algorandfoundation/tealscript';
 
 // TODO: Uncomment Line 8 before solving the puzzle.
-console.log('hit');
 // eslint-disable-next-line no-unused-vars
 class AlgorandPuzzle2 extends Contract {
   solveThePuzzle(): string {
