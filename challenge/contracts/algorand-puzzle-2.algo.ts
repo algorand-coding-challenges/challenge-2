ef83e26e29d3e3ebbd33cf67fddb1fada464a3f8
Voxiiz <thevoxiiz@gmail.com>
2024-03-17 14:54:36 +0100

    challenge 2 complete

--- challenge/contracts/algorand-puzzle-2.algo.ts
+++ challenge/contracts/algorand-puzzle-2.algo.ts
@@ -5,6 +5,6 @@ import { Contract } from '@algorandfoundation/tealscript';
 // eslint-disable-next-line no-unused-vars
 class AlgorandPuzzle2 extends Contract {
   solveThePuzzle(): string {
-    // return 'You solved the puzzle!';
+  return 'You solved the puzzle!';
   }
 }
